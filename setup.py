ca3086abc791b091aa583a126160578826736467
Ziyang Hu <hu.ziyang@cantab.net>
2018-08-27 22:20:00 +0800

    change dependency name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='pg_db_migrator',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pg_db_migrator'],
     url='',
     license='',
     author='Ziyang Hu',
     author_email='hu.ziyang@cantab.net',
     description='',
-    install_requires=['psycopg2', 'tarjan']
+    install_requires=['psycopg2-binary', 'tarjan']
 )
